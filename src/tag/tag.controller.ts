d5bf3648873d63975c68de45fd6d40fc75596f0e
TW-DEV-YangyangCAO <yycao@thoughtworks.com>
2019-09-18 16:35:30 +0800

    chore: add data filter

--- src/tag/tag.controller.ts
+++ src/tag/tag.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Get, Post } from '@nestjs/common';
 import { TagService } from './tag.service';
 import { CreateTagDto } from './dto/CreateTagDto';
 import { Tag } from './interfaces/tag.interface';
+import * as _ from 'lodash';
 
 @Controller('tag')
 export class TagController {
@@ -9,7 +10,8 @@ export class TagController {
 
     @Get()
     async getTags(): Promise<Tag[]> {
-        return this.tagService.findAll();
+         return this.tagService.findAll().then(data =>
+             data.map(dataInDb => _.pick(dataInDb, ['name', 'rgbColor', 'createDate'])));
     }
 
     @Post()
